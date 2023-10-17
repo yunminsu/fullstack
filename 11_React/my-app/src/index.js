7171ad3c33d40c21bb3776d596cafd6983fffb69
yunminsu <msg3265@nave.com>
2023-10-17 21:43:49 +0900

    10월 17일 업로드

---
+++ 11_React/my-app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import JsxUse from './chapter3/3.4/jsxUse';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    // <App />
+
+    // 3장 예제
+    // 컴포넌트: 대문자, HTML: 소문자
+    <JsxUse />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
