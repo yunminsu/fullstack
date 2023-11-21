0d4e06c23038d550b904dbb18c780adf85ee5448
yunminsu <msg3265@nave.com>
2023-11-21 17:49:46 +0900

    11월 21일 업로드

---
+++ test_js/js/arr.js
@@ -0,0 +1,14 @@
+const abc = [ 
+  {
+    id: 1,
+    name: 'jung'
+  },
+  {
+    id: 2,
+    name: 'park'
+  },
+  {
+    id: 3,
+    name: 'lee'
+  },
+]
--- test_js/js/main.js
+++ test_js/js/main.js
@@ -3,3 +3,23 @@ new Swiper('.swiper', {
   loop: true,
   autoplay: true,
 });
+
+const arr = [
+  {
+    id: 1,
+    name: '홍길동'
+  },
+  {
+    id: 2,
+    name: '김길동'
+  },
+  {
+    id: 3,
+    name: '박길동'
+  }
+]
+
+// 배열에 속성/값 추가하기
+arr[0].job = '백수'
+
+console.log(arr);
